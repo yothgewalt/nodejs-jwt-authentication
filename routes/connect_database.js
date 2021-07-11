ece7467acd91d287a210f93a4c185b5ccfc1bfdd
eine.schlacht <77398391+einemutigeschlacht@users.noreply.github.com>
2021-07-11 19:21:41 +0700

    [Uploaded] connect_database.js
    The file has been corrected.

---
+++ routes/connect_database.js
@@ -0,0 +1,11 @@
+const database = require("mysql2");
+
+const storage = database.createPool({
+    host: '',
+    user: '',
+    database: '',
+    password: '',
+    port: 3306
+});
+
+module.exports = storage.promise();
